b93321d3c07e485c286a8c50b2be75e2b6839c48
harmanmstf <harmanmstf@gmail.com>
2023-08-25 14:12:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tmdb/ui/movies/MoviesViewModel.kt
@@ -0,0 +1,37 @@
+package com.example.tmdb.ui.movies
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import com.example.tmdb.model.MovieDetailsResponse
+import com.example.tmdb.paging.MoviesPagingSource
+import com.example.tmdb.repository.ApiRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class MoviesViewModel @Inject constructor(private val repository: ApiRepository) : ViewModel() {
+
+    val loading = MutableLiveData<Boolean>()
+
+    val moviesList = Pager(PagingConfig(1)) {
+        MoviesPagingSource(repository)
+    }.flow.cachedIn(viewModelScope)
+
+
+    //Api
+    val detailsMovie = MutableLiveData<MovieDetailsResponse>()
+    fun loadDetailsMovie(id: Int) = viewModelScope.launch {
+        loading.postValue(true)
+        val response = repository.getMovieDetails(id)
+        if (response.isSuccessful) {
+            detailsMovie.postValue(response.body())
+        }
+        loading.postValue(false)
+    }
+}
